4b858d90d505285527c5bf771a8316b637774236
이기우 <rldn4743@naver.com>
2023-10-01 12:35:43 +0900

    1차

--- src/school/Student.java
+++ src/school/Student.java
@@ -3,7 +3,9 @@ package school;
 public class Student {
 
 	String name;
+	
 	int stuNum;
+	
 	public Student(String name,int stuNum) {
 		this.name=name;
 		this.stuNum=stuNum;
--- src/school/Subject.java
+++ src/school/Subject.java
@@ -5,9 +5,10 @@ public class Subject {
 	String stunName;
 	String subName;
 	int score;
+	public Subject() {
+	}
 	
 	public Subject(String stunName, String subName, int score) {
-		super();
 		this.stunName = stunName;
 		this.subName = subName;
 		this.score = score;
